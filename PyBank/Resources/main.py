d90a22c0fe8bbfb92fecf8d971a1ba588440ed79
brittanijoi94 <brittanijoi@yahoo.com>
2021-07-05 09:34:32 -0400

    python-challenge

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -7,7 +7,7 @@ csvpath = os.path.join("Pybank", "resources", "budget_data.csv")
 with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter= ",")
-    csv_reader = next(csvreader)
+    csv_reader = next(csvreader, None)
 
     dates = []
     profit_losses = []
@@ -16,6 +16,18 @@ with open(csvpath) as csvfile:
     for row in csvreader:
         dates.append(row[0])
         profit_losses.append(row[1])
+    
+    total_months = 0
+    for month in dates:
+        total_months += 1
+
+    net_total = 0
+    for row in profit_losses:
+        net_total += int(row)
+
+    for i in range(len(profit_losses)):
+        monthly_change.append(str(profit_losses[i+1]-profit_losses[i]))
+        print(monthly_change)
 
 
     
